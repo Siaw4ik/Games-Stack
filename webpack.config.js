358643e26a766412d45432a7a6d98db6092935d9
Siaw4ik <peretsviach@gmail.com>
2023-01-30 15:45:28 +0100

    init: start RS Clone task and create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const EslintPlugin = require("eslint-webpack-plugin");
+/* const CopyWebpackPlugin = require("copy-webpack-plugin"); */
+
+const config = {
+  entry: "./src/index.ts",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "./assets/[hash][ext]",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".html"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: "ts" }),
+
+    /* new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/assets/favicon.ico",
+          to: "assets/favicon.ico",
+        },
+      ],
+    }), */
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(config, envConfig);
+};
