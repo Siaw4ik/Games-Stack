3472352e6df836df78922902819b406c023b3617
Bulrock <maksim.volozhinskij@gmail.com>
2023-02-07 00:13:40 +0100

    refactor: add some text to the main page

--- src/components/gamesInfo/gamesData.ts
+++ src/components/gamesInfo/gamesData.ts
@@ -5,12 +5,16 @@ import picture3 from "../../assets/game3_picture.png"
 import picture4 from "../../assets/game4_picture.png"
 
 export const gamesData = {
+  ru_title: "Способности Силы были проявлением возможностей чувствительных к Силе существ, например, джедаев или ситхов. Учёными эти способности были описаны как сверхъестественные явления, невозможные с точки зрения науки. Существовало множество способностей Силы, однако для освоения некоторых требовалась приверженность к светлой или тёмной стороне Силы, а для познания иных был необходим высокий потенциал и постоянные тренировки разума и тела.",
+  en_title: "Force abilities were a manifestation of the capabilities of Force-sensitive beings, such as the Jedi or the Sith. Scientists have described these abilities as supernatural phenomena, impossible from the point of view of science. There were many abilities of the Force, however, the development of some required commitment to the light or dark side of the Force, and the knowledge of others required high potential and constant training of the mind and body.",
+  ru_quote: "«Джедай использует Силу для познания и защиты, никогда для нападения» — Йода",
+  en_quote: "«A Jedi uses the Force to know and protect, never to attack» — Yoda",
   "ru": [
     {
       id: "game1",
       name: "Сила мысли Джедая",
       description:
-        "Способности Силы были проявлением возможностей чувствительных к Силе существ, например, джедаев или ситхов. Учёными эти способности были описаны как сверхъестественные явления, невозможные с точки зрения науки. Существовало множество способностей Силы, однако для освоения некоторых требовалась приверженность к светлой или тёмной стороне Силы, а для познания иных был необходим высокий потенциал и постоянные тренировки разума. Для поддержания силы разума джедая и существует данное упражнение-соревнование силы мысли.",
+        "Для поддержания силы разума джедая и существует данное упражнение-соревнование силы мысли.",
       rules:
         "Правила игры: Вам необходимио за 60 секунд ответить верно на как можно большее количество вопросов, каждый правильный ответ дает вам 1 балл.",
       pictureUrl: `${picture3}`,
@@ -18,7 +22,7 @@ export const gamesData = {
     },
     {
       id: "game2",
-      name: "Джедай",
+      name: "Ловкость Джедая",
       description:
         "В данной игре вам предоставляется возможность проявить ловкость своих пальцев и быстроту реакции. Вам необходимио помочь Дино в его любимом занятии - беге с препятствиями, при подготовке к Мировому чемпионату по этой дисциплине.",
       rules:
@@ -52,7 +56,7 @@ export const gamesData = {
       id: "game1",
       name: "Jedi mind powers",
       description:
-        "Force abilities were a manifestation of the capabilities of Force-sensitive beings, such as the Jedi or the Sith. Scientists have described these abilities as supernatural phenomena, impossible from the point of view of science. There were many abilities of the Force, however, the development of some required commitment to the light or dark side of the Force, and the knowledge of others required high potential and constant training of the mind. To maintain the power of the mind of the Jedi, there is this exercise-competition of the power of thought.",
+        "To maintain the power of the mind of the Jedi, there is this exercise-competition of the power of thought.",
       rules:
         "Game rules: You need to answer as many questions correctly as possible in 60 seconds, each correct answer gives you 1 point.",
       pictureUrl: `${picture3}`,
@@ -60,7 +64,7 @@ export const gamesData = {
     },
     {
       id: "game2",
-      name: "Jedi",
+      name: "Jedi Agility",
       description:
         "In this game, you are given the opportunity to show the dexterity of your fingers and quick reaction. You need to help Dino in his favorite pastime - steeplechase, in preparation for the World Championship in this discipline.",
       rules:
