cd285d27a9731a80c51a03d8730b8174f30542e1
pahomomg <khomich001@gmail.com>
2023-02-11 15:32:43 +0300

    feat: finish game4

--- src/components/game4/index.ts
+++ src/components/game4/index.ts
@@ -1,8 +1,8 @@
 import "./index.css";
 
-const origBoard: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
-const humanPlay = "O";
-const ai = "X";
+let origBoard: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+let humanPlay: string = "";
+let ai: string = "";
 const winCombinatios = [
   [0, 1, 2],
   [3, 4, 5],
@@ -15,8 +15,11 @@ const winCombinatios = [
 ];
 
 function declareWinner(who: string) {
-  const winner = document.getElementById("game4-end") as HTMLElement;
-  winner.innerHTML = who;
+  const endBlock = document.getElementById("game4-end") as HTMLElement;
+  const endMessage = document.getElementById("game4-endMessage") as HTMLElement;
+
+  endBlock.classList.add("open");
+  endMessage.innerHTML = who;
 }
 
 function checkWin(board: string[], player: string) {
@@ -342,13 +345,6 @@ function bestSpot() {
 
 function turnClick(e: Event): void {
   function gameOver(gameWon: { index: number; player: string }) {
-    winCombinatios[gameWon.index].forEach((el) => {
-      const winSquare = document.getElementById(
-        `game4-cell-${Number(el)}`
-      ) as HTMLElement;
-      winSquare.style.backgroundColor =
-        gameWon.player === humanPlay ? "blue" : "red";
-    });
     const cells: HTMLElement[] = Array.from(
       document.querySelectorAll(".game4-main__game-cell")
     );
@@ -358,18 +354,22 @@ function turnClick(e: Event): void {
     declareWinner(gameWon.player === humanPlay ? "You win" : "You lose");
   }
   function turn(squareId: string, player: string) {
-    origBoard[Number(squareId.split("-")[2])] = player;
+    if (humanPlay) {
+      const chips = Array.from(document.querySelectorAll(".game4-main__chip"));
+      chips.forEach((el) => el.classList.add("disable"));
+      origBoard[Number(squareId.split("-")[2])] = player;
 
-    const current = document.getElementById(squareId) as HTMLElement;
-    current.innerHTML = player;
+      const current = document.getElementById(squareId) as HTMLElement;
+      current.innerHTML = player;
 
-    if (player === "X") {
-      current.classList.add("game4-dart");
-      current.removeEventListener("click", turnClick, false);
-    }
-    if (player === "O") {
-      current.classList.add("game4-yoda");
-      current.removeEventListener("click", turnClick, false);
+      if (player === "game4-yoda-chip") {
+        current.classList.add("game4-yoda");
+        current.removeEventListener("click", turnClick, false);
+      }
+      if (player === "game4-dart-chip") {
+        current.classList.add("game4-dart");
+        current.removeEventListener("click", turnClick, false);
+      }
     }
   }
   function checkTie() {
@@ -379,7 +379,6 @@ function turnClick(e: Event): void {
         document.querySelectorAll(".game4-main__game-cell")
       );
       cells.forEach((element) => {
-        element.style.backgroundColor = "green";
         element.removeEventListener("click", turnClick, false);
       });
       declareWinner("Tie Game");
@@ -409,14 +408,19 @@ function turnClick(e: Event): void {
 export function startGame() {
   const mainGame = document.querySelector(".game4-main__container");
   if (mainGame) {
+    const chips = Array.from(document.querySelectorAll(".game4-main__chip"));
+    chips.forEach((el) => el.classList.remove("disable"));
     const cells: HTMLElement[] = Array.from(
       document.querySelectorAll(".game4-main__game-cell")
     );
     cells.forEach((e) => {
       e.innerHTML = "";
-      e.style.removeProperty("background-color");
-      e.addEventListener("click", turnClick, false);
+      e.classList.remove("game4-yoda", "game4-dart");
+      e.removeEventListener("click", turnClick, false);
     });
+    origBoard = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    ai = "";
+    humanPlay = "";
   }
 }
 export function game4() {
@@ -424,8 +428,15 @@ export function game4() {
   main.innerHTML = "";
   const body = document.createElement("div");
   body.classList.add("game4-wrapper");
-  body.innerHTML = `    <div class="game4-wrapper">
+  body.innerHTML = `
   <div class="game4-main__container _game4-container">
+    <div class="game4-main__settings">
+      <h1 class="game4-main__title">To play Tic-Tac-Toe choose a chip</h1>
+      <div class="game4-main__chips">
+        <div id="game4-yoda-chip" class="game4-main__chip"></div>
+        <div id="game4-dart-chip" class="game4-main__chip"></div>
+      </div>
+    </div>
     <div id="game4-main" class="game4-main__game-table">
       <div class="game4-main__game-row">
         <div id="game4-cell-0" class="game4-main__game-cell"></div>
@@ -442,10 +453,43 @@ export function game4() {
         <div id="game4-cell-7" class="game4-main__game-cell"></div>
         <div id="game4-cell-8" class="game4-main__game-cell"></div>
       </div>
+      <div id="game4-end" class="game4-main__endgame game4-endgame">
+    <div id="game4-endMessage" class="game4-endgame__message"></div>
+    <button id="game4-retryBtn" class="game4-endgame__button">Retry</button>
     </div>
-    <div id="game4-end" class="game4-main__endgame"></div>
-  </div>
-</div>`;
+    </div>
+  </div>`;
   main.appendChild(body);
   startGame();
+}
+
+export function retryBtnclick() {
+  const main = document.querySelector(".main") as HTMLElement;
+
+  main.addEventListener("click", (e) => {
+    if ((<HTMLButtonElement>e.target).id === "game4-retryBtn") {
+      const endBlock = document.getElementById("game4-end") as HTMLElement;
+      endBlock.classList.remove("open");
+      startGame();
+    }
+  });
+}
+
+export function chipClick() {
+  const main = document.querySelector(".main") as HTMLElement;
+  main.addEventListener("click", (e) => {
+    if ((<HTMLElement>e.target).className === "game4-main__chip") {
+      humanPlay = (<HTMLElement>e.target).id;
+    }
+    if (humanPlay === "game4-yoda-chip") ai = "game4-dart-chip";
+    else {
+      ai = "game4-yoda-chip";
+    }
+    const cells: HTMLElement[] = Array.from(
+      document.querySelectorAll(".game4-main__game-cell")
+    );
+    cells.forEach((el) => {
+      el.addEventListener("click", turnClick, false);
+    });
+  });
 }
