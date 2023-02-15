d3387fc268a2ebea217ba34cbd918e06502d5564
Siaw4ik <peretsviach@gmail.com>
2023-02-15 11:47:27 +0100

    feat: make 8 Unit test

--- tests/components/slawik.test.ts
+++ tests/components/slawik.test.ts
@@ -2,6 +2,8 @@ import { changeScore20 } from "../../src/components/game5/componentsGame5";
 import { createObjParametr } from "../../src/components/results/componentsResault";
 import { StatisticGames } from "../../src/components/module/Games";
 import { checkValidLoginSignUP } from "../../src/components/registration/сomponentsRegister";
+import { checkValidPasswordSignUP } from "../../src/components/registration/сomponentsRegister";
+import { User } from "../../src/components/module/User";
 
 describe("changeScore20 function:", () => {
   test("should return the score minus 20", () => {
@@ -43,25 +45,24 @@ describe("createObjParametr function:", () => {
 });
 
 const statistic = new StatisticGames();
+const user = new User();
 
 const fetchReturnSlawik = {
-  success: true,
-  username: "Slawik",
-  scores: {
-    "Jedi Agility": 105628,
-    "Jedi's Memory": 810,
-    "Jedi's Mobility": 250,
-    "Jedi's Agiclity": 100,
-    "Jedi's Agility": 45,
-    "Jedi's Strategy": 19,
-    "Jedi's Mind": 14,
-  },
+  "success": true,
+  "username": "TestGameStack001",
+  "scores": {
+      "Jedi's Memory": 260,
+      "Jedi's Agility": 44,
+      "Jedi's Mobility": 36,
+      "Jedi's Mind": 11,
+      "Jedi's Strategy": 4
+  }
 };
 
 describe("StatisticGames.getScoreGamesUser function:", () => {
   test("should return games with results played by user Slavik", async () => {
     const data = await statistic.getScoreGamesUser({
-      username: "Slawik",
+      username: "TestGameStack001",
       option: "ascScore",
     });
     expect(data).toEqual(fetchReturnSlawik);
@@ -81,11 +82,60 @@ describe("StatisticGames.getScoreTop10 function:", () => {
   });
 });
 
+describe("user.reсordUser function:", () => {
+  test("should return a response from the server stating that this user already exists on the server", async () => {
+    const data = await user.reсordUser({
+      username: "Slawik",
+      password: "slawacool"
+  });
+    expect(data).toEqual({
+      success: false,
+      message: "Username is already exists"
+  });
+  });
+});
+
+const successLogin = {
+  "success": true,
+  "user": {
+      "_id": "63ecaff846cea8759fe949a9",
+      "username": "TestGameStack001",
+      "__v": 0,
+      "password": "testgamestack001",
+      "highScores": {
+          "Jedi's Mind": 11,
+          "Jedi's Agility": 44,
+          "Jedi's Mobility": 36,
+          "Jedi's Strategy": 4,
+          "Jedi's Memory": 260
+      }
+  }
+};
+
+describe("user.loginUser function:", () => {
+  test("should return a response from the server stating that the user has successfully registered on the site", async () => {
+    const data = await user.loginUser({
+      username: "TestGameStack001",
+      password: "testgamestack001"
+  });
+    expect(data).toEqual(successLogin);
+  });
+});
+
 describe("checkValidLoginSignUP function:", () => {
   test("return a boolean value, corresponding to the validation login string longer than 5 characters and have capital letters", () => {
     expect(checkValidLoginSignUP("Slawik")).toBe(true);
     expect(checkValidLoginSignUP("slawa")).toBe(false);
     expect(checkValidLoginSignUP("s")).toBe(false);
     expect(checkValidLoginSignUP("SlawikCool")).toBe(true);
+  });
+});
+
+describe("checkValidPasswordSignUP function:", () => {
+  test("return a boolean value, corresponding to the validation password string longer than 5 characters", () => {
+    expect(checkValidPasswordSignUP("Slawik")).toBe(true);
+    expect(checkValidPasswordSignUP("slawaaa")).toBe(true);
+    expect(checkValidPasswordSignUP("s")).toBe(false);
+    expect(checkValidPasswordSignUP("")).toBe(false);
   });
 });
